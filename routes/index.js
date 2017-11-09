4fd0964cda3bcf3db63c146d654c26aa515f8417
Seyed Majed Shobbar <Majed1358@yahoo.com>
2017-11-08 18:49:46 -0800

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.post('/savedata', function (req, res, next) {
     //res.send("hello " + value_name);
 
         //var arr = req.body;
-        res.send('Response is 7 '+ req.body.session_basket+' -- '+ shipment_info[0].add1+' -- '+shipment_info[0]);
+        res.send('Response is 7 '+ req.body.session_basket+' -- '+ shipment_info+' -- '+shipment_info[0]);
 
 });
 
