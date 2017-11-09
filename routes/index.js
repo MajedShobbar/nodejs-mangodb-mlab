55a9c651d3e4ae5e69cbf2e45bc6d218ebe27f75
Seyed Majed Shobbar <Majed1358@yahoo.com>
2017-11-08 21:08:51 -0800

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -93,7 +93,7 @@ router.post('/savedata', function (req, res, next) {
 
         //var session_basketString = JSON.stringify(req.body.payment_info);
         //res.send('Your order has been saved and will process shortly');
-        res.sendFile('views/mongodb.ejs', {root: __dirname })
+        res.sendFile('views/test.ejs', {root: __dirname })
 
         //close connection when your app is terminating.
         db.close(function (err) {
